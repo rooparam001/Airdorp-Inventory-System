e5a690ef3446a8dc320557c3c8eaa80ea503bf2d
rooparam001 <rambeniwal151996@gmail.com>
2024-06-19 07:38:58 +0530

    First Commit of All files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AirdropInventoryApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
